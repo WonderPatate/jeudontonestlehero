5c51a65c6ab8080587c5fd8e3e34272045b6a550
Wonder Potato <marinegarguy@gmail.com>
2020-07-25 11:27:19 +0200

    duplication dossier exo asp.net core

---
+++ STAGE/jeudontonestlehero/jeudontonestlehero.Core.data/Models/Aventure.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jeudontonestlehero.Core.data
+{
+    public class Aventure
+    {
+        #region Propriétés
+        public int Id { get; set; }
+
+        public string Titre { get; set; }
+
+        #endregion
+    }
+}
---
+++ STAGE/jeudontonestlehero/jeudontonestlehero.web.UI/Controllers/AventureController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using jeudontonestlehero.web.UI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace jeudontonestlehero.web.UI.Controllers
+{
+    public class AventureController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.MonTitre = "Aventures";
+
+            List<Aventure> maList = new List<Aventure>();
+
+            maList.Add(new Aventure()
+            {
+                Id = 1,
+                Titre = "Ma première aventure"
+            });
+
+            maList.Add(new Aventure()
+            {
+                Id = 2,
+                Titre = "Ma seconde aventure"
+            });
+
+            return View(maList);
+        }
+
+        
+    }
+}
